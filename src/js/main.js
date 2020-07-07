dce340c3ee5e3b0e4a606bd2ee5320e96e79ab4e
Shridhar Ulli <shridharulli@gmail.com>
2020-07-07 15:51:22 +0530

    testimonial students responsive

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,28 @@ var swiper = new Swiper("#testimonials_teachers", {
     clickable: true,
   },
 });
+
 var swiper = new Swiper("#testimonials_students", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 40,
+  // init: false,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
   },
 });
 
