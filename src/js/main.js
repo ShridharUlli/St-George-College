35800361a3975334d16cd19bff0bb22c4f524aee
Shridhar Ulli <shridharulli@gmail.com>
2020-07-11 21:56:16 +0530

    images

--- src/js/main.js
+++ src/js/main.js
@@ -128,3 +128,10 @@ $("document").ready(function () {
 });
 
 AOS.init();
+
+//scroll to top
+
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
