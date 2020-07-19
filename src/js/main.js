6854490948a58d705412f5eb1239a584b5dc4803
Shridhar Ulli <shridharulli@gmail.com>
2020-07-19 15:11:33 +0530

    changes

--- src/js/main.js
+++ src/js/main.js
@@ -226,10 +226,10 @@ var openmodal = document.querySelectorAll('.modal-open')
 
     const callToAction = document.querySelector("#call_to_action");
     const icon = document.querySelector("#call_to_action > i ");
-  
+    
     const callToActionDetails = document.querySelector("#call_to_action_details");
     const callUs = document.querySelector("#call_us");    
-
+    
     function displayCallToAction(){
       if(callToActionDetails.classList.contains('hidden')){
         callToActionDetails.classList.remove('hidden');
@@ -246,40 +246,28 @@ var openmodal = document.querySelectorAll('.modal-open')
     function displayDialView(){
       callUs.innerHTML = `<div class="flex items-center justify-around p-2">
       <div class=" flex-grow-0 h-10 flex items-center justify-center">   
-        <div class="text-md text-gray-700 uppercase" id="dial_us">dial us</div>      
+      <div class="text-md text-gray-700 uppercase" id="dial_us">dial us</div>      
       </div>
       <div class="flex-grow-0 h-10 w-px bg-gray-700"></div>
       <div class="flex-grow-0 flex items-center justify-center">
-        <div class="text-md text-gray-700 uppercase" id="call_back">call back</div>
-        
+      <div class="text-md text-gray-700 uppercase" id="call_back">call back</div>
+      
       </div>
-    </div>`;
-
-    
-   
-
+      </div>`;
+      const callBack = document.querySelector("#call_back");
+      callBack.addEventListener("click", callBackForm);
     }
-
-   
+    
+    function callBackForm(){
+      callUs.innerHTML = `<div class="flex items-center justify-around p-2">
+      <div class="h-10 flex items-center justify-center">   
+      <input type="text" class=""/> 
+      <h1>hellow world</h1>
+      </div      
+      </div>`;
+    }
 
     callToAction.addEventListener("click", displayCallToAction);
     callUs.addEventListener("click", displayDialView);
-    
-    
 
-    const callBack = document.querySelector("#call_back");
-    
-    // function callBackForm(){
-
-    //   callUs.innerHTML = `<div class="flex items-center justify-around p-2">
-    //   <div class="h-10 flex items-center justify-center">   
-    //   <input type="text" class=""/> 
-    //   <h1>hellow rold</h1>
-    //   </div      
-    //   </div>`;
-    // }
-    
-    callBack.onload = alert("loaded");
-    
-    
     
